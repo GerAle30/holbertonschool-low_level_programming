7cbaa32dbdcd2668988d5a3cc0bdfbb70be1bc75
GerAle30 <garciaslj@gmail.com>
2025-02-20 11:16:13 -0400

    2-printalpahbet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -print the alphabet x10
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int abc, cou;
+
+	/*this count the amount of time. the alphabet is printed*/
+	cou = 0;
+	while (cou < 10)
+	{
+		/* this print the alphabet with a newlines*/
+		for (abc - 'a'; abc <= 'z' ; abc++)
+			_putchar(abc);
+		_putchar('\n');
+		cou++;
+	}
+}
