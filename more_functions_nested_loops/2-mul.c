b6069fa0a1ac8a1f7caa8201e1800a1b9fc7cd42
GerAle30 <garciaslj@gmail.com>
2025-02-26 15:05:03 -0400

    2-multest for the code

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - multiple two integer
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
