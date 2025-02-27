ce87248a8457d4d1e21b757e6869e6addd8c9a5d
GerAle30 <garciaslj@gmail.com>
2025-02-27 13:53:58 -0400

    9-FizzBuzzresolvespace

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,22 @@ int main(void)
 		{
 			printf(" ");
 		}
-		
+
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		
+
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		
+
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		
+
 		else
 		{
 			printf("%d", i);
