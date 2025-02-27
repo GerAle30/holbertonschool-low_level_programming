14ef9a2911c7a33a0a87726f1569b69993899e9a
GerAle30 <garciaslj@gmail.com>
2025-02-27 10:31:08 -0400

    5-more_numbers resolve

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * more_numbers - prints numbers from 0 to 14
- * 10 times,each on a new line 
+ * 10 times,each on a new line
  */
 void more_numbers(void)
 {
 	int o;
 	int u;
 
-	for (o = 0; o <10; o++)
+	for (o = 0; o < 10; o++)
 	{
 		for (u = 0; u <= 14; u++)
 		{
