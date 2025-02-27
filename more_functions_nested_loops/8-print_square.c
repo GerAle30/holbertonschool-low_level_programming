62b3385f811675256cfc86c43d9836eac30e0e13
GerAle30 <garciaslj@gmail.com>
2025-02-27 11:45:35 -0400

    8-printsquarefirstest

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * Print_square - -prints a square of '#' characters
+ * @size: size pf the square
+ */
+void print_square(int size)
+{
+	int i, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
