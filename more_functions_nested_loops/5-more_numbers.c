c99457a2e0dad12231e55e53eac6d444b11e91ff
GerAle30 <garciaslj@gmail.com>
2025-02-27 10:16:43 -0400

    5morenumbers commit write wrong

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14
+ * 10 times,each on a new line 
+ */
+void more_numbers(void)
+{
+	int o;
+	int u;
+
+	for (o = 0; o <10; o++)
+	{
+		for (u = 0; u <= 14; u++)
+		{
+			if (u >= 10)
+			{
+				_putchar((u / 10) + '0');
+			}
+			_putchar((u % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
