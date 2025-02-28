eecd2fd619c369cc6c50a40530b62299d6317c53
GerAle30 <garciaslj@gmail.com>
2025-02-28 11:43:53 -0400

    2-strlentest

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @str: pointer to the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	return (length);
+}
