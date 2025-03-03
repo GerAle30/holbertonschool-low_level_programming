bf16fa17b4c48c7f35fe8e84e4e14c6976187668
GerAle30 <garciaslj@gmail.com>
2025-03-02 23:27:29 -0400

    7-leetproblem resolve

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337 (leet) speak
- * @str: the string to be modified
+ * leet - encodes a string into 1337 (leet) speak.
+ * @str: the string to be modified.
  *
- * Return: the modified string (encoded to 1337)
+ * Return: the modified string (encoded to 1337).
  */
 char *leet(char *str)
 {
 	int i, j;
-	char leet_chars[] = {'a', 'e', 'o', 't', '1'};
+	char leet_chars[] = {'a', 'e', 'o', 't', 'l'};
 	char leet_replacements[] = {'4', '3', '0', '7', '1'};
 
 	/* Iterate through each character in the string */
@@ -18,7 +18,7 @@ char *leet(char *str)
 		/* Compare each character with the 'leet' characters */
 		for (j = 0; j < 5; j++)
 		{
-			/* If a match is found, replace the characters */
+			/* If a match is found, replace the character */
 			if (str[i] == leet_chars[j] || str[i] == leet_chars[j] - 32)
 			{
 				str[i] = leet_replacements[j];
@@ -26,5 +26,7 @@ char *leet(char *str)
 			}
 		}
 	}
-	return (str);
+
+	return (str); /* Return the modified string */
 }
+
