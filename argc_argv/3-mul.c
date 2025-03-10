cfb25e2a237299763188c3132cf57b1c027d1505
GerAle30 <garciaslj@gmail.com>
2025-03-10 13:37:02 -0400

    mul fix the error

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers and prints the result
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: 0 on sucess, or 1 if the arguments are not correct.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
