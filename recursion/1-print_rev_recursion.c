7457adea60d8baf31b8511d20aeabd3836d87d7e
GerAle30 <garciaslj@gmail.com>
2025-03-10 00:28:39 -0400

    1-print_rev_recursion try to found one proble 1test

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recurison - Prints a string in reverse using recursion
+ * @s: The string to be printed
+ *
+ * return: Nothing
+ */
+void _print_rev_recursion(char*s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1); /* Recursive call with next character */
+		_putchar(*s); /* Print the current character */
+	}
+}
