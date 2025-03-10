537c0e30d862af2503145c260b8e1073200a1277
GerAle30 <garciaslj@gmail.com>
2025-03-10 01:02:22 -0400

    pow_recursion 1test

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the vlaue of 'x' raised to the power pf 'y'
+ * @x: The base number
+ * @y: the exponent
+ *
+ * Return: The result of 'x' raised to the power pf 'y', 
+ * or -1 if 'y' is negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
