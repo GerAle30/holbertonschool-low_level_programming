d74d08ee4d9e432a18749652ac9695f13b687039
GerAle30 <garciaslj@gmail.com>
2025-03-16 14:03:31 -0400

    mensaje

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,17 +13,22 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t len1, len2, i;
+	unsigned int len1 = 0, len2 = 0, i;
 	char *concat;
 
 	/* Handle NULL inputs */
-	s1 = (s1 == NULL) ? "" : s1;
-	s2 = (s2 == NULL) ? "" : s2;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	/* Calculate lengths manually (without strlen) */
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-	/* If n is greater than s2 length, use full s2 */
+	/* If n is grater than s2 lenght, use full s2 */
 	if (n > len2)
 		n = len2;
 
@@ -32,14 +37,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!concat)
 		return (NULL);
 
-	/*Copy s1 and first n characters of s2 into concat */
+	/* Copy s1 into concat */
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
+
+	/* Copy up to n characters from s2 */
 	for (; i < len1 + n; i++)
 		concat[i] = s2[i - len1];
 
-	concat[i] = s2[i - len1];
+			concat[i] = '\0'; /* NULL-terminate the string */
 
-	concat[i] = '\0'; /* NULL-terminate the string */
 	return (concat);
 }
