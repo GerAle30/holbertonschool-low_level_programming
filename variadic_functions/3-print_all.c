1d9d6886dcfbfad869ea15018450bd2f8e695b54
GerAle30 <garciaslj@gmail.com>
2025-03-23 21:52:02 -0400

    mensaje

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -20,29 +20,31 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' ||
-				format[i] == 'f' || format[i] == 's')
+		switch (format[i])
 		{
-			printf("%s", sep);
-			sep = ",";
-
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			else if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			else if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			else if (format[i] == 's')
-			{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
 				str = va_arg(args, char *);
-				if (str == NULL)
+				if (!str)
 					str = "(nil)";
-				printf("%s", str);
-			}
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		sep = ", ";
 		i++;
 	}
+
 	va_end(args);
 	printf("\n");
 }
-
