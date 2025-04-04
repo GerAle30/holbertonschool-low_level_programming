2e0cc0ac0c1d6625f039077610473f350dd16fe9
GerAle30 <garciaslj@gmail.com>
2025-04-03 22:27:12 -0400

    mensaje

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  * print_list - Prints all the elements of a list_t list.
  * @h: Pointer to the head od the linked list.
  *
- * return: The number of nodes in the list.
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
