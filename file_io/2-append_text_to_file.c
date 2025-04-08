31c1a37832ef1a969faa3e0427c96226694eff28
GerAle30 <garciaslj@gmail.com>
2025-04-08 14:12:57 -0400

    mensaje

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
- * text_content: NULL-terminated string to add at the end of the file
+ *@text_content: NULL-terminated string to add at the end of the file
  *
  * Return: 1 on sucess, -1 on failure
  */
